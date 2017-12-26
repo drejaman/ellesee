c1afcb51a23b6c1d4126ce27eb58e004c40f912b
Noor Zaman <noorzaman@Noors-MacBook-Air.local>
2017-12-26 14:26:13 -0800

    LC Island Perimeter

--- src/arrays/TestArrays.java
+++ src/arrays/TestArrays.java
@@ -37,4 +37,45 @@ public class TestArrays {
      
      return reshapedMatrix;
     }
+    
+    public int islandPerimeter(int[][] grid) {
+    		if(grid == null) return 0;
+    		
+    		int row = grid.length;
+    		int col = grid[0].length;
+    		int perimeter = 0;
+    		
+    		for(int i = 0; i < row; i++)
+    			for(int j = 0; j < col; j++)
+    			{
+    				if(grid[i][j] == 1)
+    				{
+    					//scan the surrounding and based on that add perimeter value
+
+    					// cell is the top or in the last row
+    					if(i == 0) perimeter++;
+    					if(i == (row - 1)) perimeter++;
+
+    					// cell is the first or in the last column
+    					if(j == 0) perimeter++;
+    					if(j == (col - 1)) perimeter++;
+    					
+    					// not the top row. check the immediate top cell
+    					if(i != 0 && grid[i-1][j] == 0) perimeter++;
+    					
+    					// not the bottom row. check immediate cell below
+    					if(i != (row - 1) && grid[i+1][j] == 0) perimeter++;
+    					
+    					// not the leftmost column. check the immediate 
+    					if(j != 0 && grid[i][j-1] == 0) perimeter++;
+    					
+    					// not the rightmost column
+    					if(j != (col - 1) && grid[i][j+1] == 0) perimeter++;
+    				}
+    			}
+    		
+    		return perimeter;
+    }
+
+
 }
